50b16ff9cb12fcc27571ca069bd8ffaef58da388
emil-svensson <114189223+emil-svensson@users.noreply.github.com>
2023-12-21 14:16:07 -0500

    Update app.py
    Mostly cosmetic minor changes

--- app.py
+++ app.py
@@ -2,12 +2,20 @@ import streamlit as st
 import pandas as pd
 from app_fncs import *
 
+## layout stuff
+st.set_page_config(layout="wide")
+colwidths = [0.6, 0.4]
+
+## more stuff at the bottom
+
+
+
 
 ## Print the title at the tippy-top of the page
-# st.set_page_config(layout="wide")
-st.markdown("""<style> .block-container {padding-top: 0 !important} </style> """, unsafe_allow_html=True)
 st.title("🌈 SWEPR 🐢")
-
+st.write(' ')
+st.write(' ')
+st.write(' ')
 
 df = load_data()
 
@@ -26,22 +34,20 @@ else:
 ## Global setting for english -> swedish, or swedish -> english
 language_mode_dict = {'English':0, 'Swedish':1}
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns(colwidths)
 with col1:
     language_mode = st.radio('Translate from...', language_mode_dict.keys(), horizontal=True, key="sel_language")
-    st.markdown('''<style> [data-testid="column"] 
-                    {width: calc(50% - 1rem) !important;
-                     flex:  1 calc(50% - 1rem) !important;
-                     min-width: calc(50% - 1rem) !important;} </style>''', unsafe_allow_html=True)
-
 with col2:
-    st.markdown("""<style> .block-container {padding-top: 20 !important} </style> """, unsafe_allow_html=True)
-    st.markdown('''<style> [data-testid="column"] 
-                {width: calc(50% - 1rem) !important;
-                    flex:  1 calc(50% - 1rem) !important;
-                    min-width: calc(50% - 1rem) !important;} </style>''', unsafe_allow_html=True)
+    st.write(' ')
+    st.write(' ')
     if st.button('Reset'):
         initialize_reset_app(st_ss)
+
+
+st.write(' ')
+st.write(' ')
+st.write(' ')
+
 
 
 global col_from, col_to, to_language
@@ -67,19 +73,19 @@ def process_user_input():
     ## Reset the st_ss user input (clear the textbox)
     if st_ss.user_input_noun != '':
         st_ss.current_word_class = 'noun'
-        user_input_temp = st_ss.user_input_noun
+        user_input_temp = st_ss.user_input_noun.strip()
         st_ss.user_input_noun = ''
     elif st_ss.user_input_verb != '':
         st_ss.current_word_class = 'verb'
-        user_input_temp = st_ss.user_input_verb
+        user_input_temp = st_ss.user_input_verb.strip()
         st_ss.user_input_verb = ''
     elif st_ss.user_input_prep != '':
         st_ss.current_word_class = 'preposition'
-        user_input_temp = st_ss.user_input_prep
+        user_input_temp = st_ss.user_input_prep.strip()
         st_ss.user_input_prep = ''
     elif st_ss.user_input_adj != '':
         st_ss.current_word_class = 'adjective'
-        user_input_temp = st_ss.user_input_adj
+        user_input_temp = st_ss.user_input_adj.strip()
         st_ss.user_input_adj = ''
 
     word_class = st_ss.current_word_class
@@ -100,28 +106,28 @@ def process_user_input():
 
 def user_facing_stuff(word_class):
 
-    col_1a, col_2a = st.columns(2)
+    col_a, col_b = st.columns(colwidths)
 
-    with col_2a:
-        st.markdown("""<style> .block-container {padding-top:  !important;} </style> """,
-            unsafe_allow_html=True)
+    with col_b:
         
         ## After the button has been pressed once, show the remaining number of words
         if st_ss.number_of_words_left[word_class] < 0:
-            button_text = f'New {word_class}'
+            button_text = f'New word'
         else:
-            button_text = f'New {word_class} ({st_ss.number_of_words_left[word_class]})'
-        if st.button(button_text):
+            button_text = f'Next! ({st_ss.number_of_words_left[word_class]})'
+
+        st.write(' ')
+        if st.button(button_text ,key=f'button_{word_class}'):
             new_word(df, st_ss, word_class)
             st_ss.user_input_status[word_class] = 'Awaiting answer'
             st.rerun()
     
-    with col_1a:
+    with col_a:
         if st_ss.current_word_idx[word_class] is not None:
             current_word = df.loc[st_ss.current_word_idx[word_class], language_keys[0]]
             st.markdown(f"## {current_word}")
         else:
-            st.markdown(f"## {' → click button →'}")
+            st.markdown(f" {'___'}")
     
     ## User input text box
     if st_ss.current_word_idx[word_class] is not None:
@@ -161,10 +167,29 @@ with tab2:
     word_class='verb'
     user_facing_stuff(word_class)
 
-# try:
-#     if msg is not None:
-#         st.write(msg)
-# except:
-#     pass
+
+
+st.divider()
+
+
+
+
+## more layout stuff
+st.markdown("""<style> .block-container {padding-top: 2rem !important;
+                                         padding-left: 0.5rem !important;
+                                         padding-right: 0.5rem !important;} </style> """,
+            unsafe_allow_html=True)
+
+st.markdown(""" <style> [data-testid="stVerticalBlock"] {gap: 0.5rem !important} </style>""",
+            unsafe_allow_html=True)
+
+st.write('''<style>
+            [data-testid="column"] {
+                min-width: calc(30% - 0.5rem) !important;} </style>''',
+        unsafe_allow_html=True)
+
+st.markdown("""<style> [data-testid="baseButton-secondary"] {width: 7rem  !important;} </style> """,
+            unsafe_allow_html=True)
+
 
 st_ss.counter += 1
